ab56f2b628067467434014f47b85559ccd72ef31
Harshdeep Bhatia <harshdeep.bhatia@gemsessence.com>
2024-08-31 17:53:46 +0530

    Implement code for managing two numbers

--- calculator.rb
+++ calculator.rb
@@ -2,5 +2,7 @@ class StringCalculator
   def add(numbers_str)
     return 0 if numbers_str.empty?
     return numbers_str.to_i if numbers_str.length == 1
+
+    numbers_str.split(',').map(&:to_i).sum
   end
 end
