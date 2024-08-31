7a7565ce24271fb1bc0e65b4e92b12be479d9018
Harshdeep Bhatia <harshdeep.bhatia@gemsessence.com>
2024-08-31 18:10:06 +0530

    manage code for delimeter case

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,13 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-
-    numbers.split(',').map(&:to_i).sum
+  
+    delimiter = /,|\n/
+    if numbers.start_with?("//")
+      delimiter = Regexp.escape(numbers[2])
+      numbers = numbers.split("\n", 2)[1]
+    end
+  
+    numbers.split(/#{delimiter}/).map(&:to_i).sum
   end
 end
