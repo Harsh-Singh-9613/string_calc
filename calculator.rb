434196fc4c85d99ff94227ff727740c5174cb220
Harshdeep Bhatia <harshdeep.bhatia@gemsessence.com>
2024-08-31 18:13:41 +0530

    manage negative number and code fixes

--- calculator.rb
+++ calculator.rb
@@ -1,13 +1,19 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-  
-    delimiter = /,|\n/
+
     if numbers.start_with?("//")
-      delimiter = Regexp.escape(numbers[2])
+      delimiter = numbers[2]
       numbers = numbers.split("\n", 2)[1]
+    else
+      delimiter = /,|\n/
     end
-  
-    numbers.split(/#{delimiter}/).map(&:to_i).sum
+
+    nums = numbers.split(/#{delimiter}|,/).map(&:to_i)
+    negatives = nums.select { |n| n < 0 }
+    raise "Negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
+
+    nums.sum
   end
+  
 end
