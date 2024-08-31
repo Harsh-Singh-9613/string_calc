92a7ffb50dd33f71a4b05eb6914820d9d1864259
Harshdeep Bhatia <harshdeep.bhatia@gemsessence.com>
2024-08-31 18:09:46 +0530

    added specs for delimeter

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -21,4 +21,14 @@ RSpec.describe StringCalculator do
     calculator = StringCalculator.new
     expect(calculator.add("1,2,3,4")).to eq(10)
   end
+
+  it "handles new lines as delimiters" do
+    calculator = StringCalculator.new
+    expect(calculator.add("1\n2,3")).to eq(6)
+  end
+
+  it "handles custom delimiters" do
+    calculator = StringCalculator.new
+    expect(calculator.add("//;\n1;2")).to eq(3)
+  end
 end
