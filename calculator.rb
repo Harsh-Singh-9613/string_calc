8174c699a491e3685bab6ea2e3fcb0cea37566d0
Harshdeep Bhatia <harshdeep.bhatia@gemsessence.com>
2024-08-31 18:01:43 +0530

    write specs for multiple number and opitmize the code

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,7 @@
 class StringCalculator
-  def add(numbers_str)
-    return 0 if numbers_str.empty?
-    return numbers_str.to_i if numbers_str.length == 1
+  def add(numbers)
+    return 0 if numbers.empty?
 
-    numbers_str.split(',').map(&:to_i).sum
+    numbers.split(',').map(&:to_i).sum
   end
 end
