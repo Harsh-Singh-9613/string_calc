f7271d210e3e9ccfd3ddf747bafc915bf0f08785
Harshdeep Bhatia <harshdeep.bhatia@gemsessence.com>
2024-08-31 17:46:56 +0530

    added rspec for empty string

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,5 +1,9 @@
+require 'rspec'
 require_relative '../calculator'
 
 RSpec.describe StringCalculator do
-  
+  it "returns 0 for an empty string" do
+    calculator = StringCalculator.new
+    expect(calculator.add("")).to eq(0)
+  end
 end
